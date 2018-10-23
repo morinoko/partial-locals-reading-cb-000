d008e2fa5a57f0aaea8e6f6e8a237031f1227ebb
Felice Forby <feliceforby@gmail.com>
2018-10-23 22:46:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :author
+  resource :post
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
